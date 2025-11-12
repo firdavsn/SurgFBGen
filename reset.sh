b22d1b9cf63826b86f04a9ccdb26d2cc4809c80c
firdavsn <firdavsnasriddinov7@gmail.com>
2025-11-11 21:48:47 -0800

    updated reset.sh

--- reset.sh
+++ reset.sh
@@ -1,3 +1,11 @@
+if [[ -n "${BASH_SOURCE[0]}" ]]; then
+    SCRIPT_PATH=$(realpath "${BASH_SOURCE[0]}")
+else
+    SCRIPT_PATH=$(realpath "$0")
+fi
+REPO_DIR=$(realpath $(dirname $SCRIPT_PATH))
+
+cd $REPO_DIR
 deactivate
 rm -rf .venv
 rm -rf SurgVLP
